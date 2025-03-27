03f112feca7f24b7e95941ff7cbfb213cb95a9d3
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-03-27 21:05:08 +0530

    Add files via upload

---
+++ Goldman Sach/max subarray sum.py
@@ -0,0 +1,16 @@
+def kadane(arr):
+    max_sum= float('-inf')
+    curr_sum= 0
+
+    for num in arr:
+        curr_sum += num
+        max_sum = max(max_sum, curr_sum)
+
+        if curr_sum <0:
+            curr_sum=0
+
+    return max_sum
+
+
+arr= [-2,3,4]
+print(kadane(arr))
