dc5c04b96e6f3594a94963bfe950ae7a8c90e41d
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-10-30 01:03:35 +0530

    Add files via upload

---
+++ ZS/Common elements.py
@@ -0,0 +1,8 @@
+def find_common(arr1,arr2,arr3):
+    return list(set(arr1) & set(arr2) & set(arr3))
+
+arr1=[2,3,5,6,7]
+arr2=[4,5,6,7,8]
+arr3=[5,6,10,12,56]
+res= find_common(arr1,arr2,arr3)
+print(res)
